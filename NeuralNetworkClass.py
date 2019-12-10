5310068fbb497c4de2a3e408b8eccceec943e9da
tom501 <luca.tomasetti92@gmail.com>
2019-12-10 21:59:12 +0100

    fix at the end of the training

--- NeuralNetworkClass.py
+++ NeuralNetworkClass.py
@@ -183,11 +183,6 @@ class NeuralNetwork(object):
                 initial_epoch=self.initial_epoch,
                 use_multiprocessing=self.mp)
 
-        # TODO: check what it is
-        print(self.train.history)
-        self.training_score = round(self.train.history[self.loss["name"]][self.epochs-1], 6)
-        self.loss_val = round(self.train.history["loss"][self.epochs-1], 6)
-
         # plot the loss and accuracy of the training
         training.plotLossAndAccuracy(self, p_id)
 

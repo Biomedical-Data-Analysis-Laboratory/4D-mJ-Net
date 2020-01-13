0b75a833d79347f4a6b1386fcdd9cc5c26206dc7
tom501 <luca.tomasetti92@gmail.com>
2020-01-13 23:42:10 +0100

    change the disposition of the dataset to savegit add get_complete_training_data.py  <-- IMPORTANT

--- get_complete_training_data.py
+++ get_complete_training_data.py
@@ -48,8 +48,9 @@ DATA_AUGMENTATION = True
 
 
 M, N = int(IMAGE_WIDTH/32), int(IMAGE_HEIGHT/32)  # 32, 32
-SLICING_PIXELS = int(M/4)
-MAX_NUM_BACKGROUND_IMAGES = 150
+SLICING_PIXELS = int(M/4) # 8
+# MAX_NUM_BACKGROUND_IMAGES = 600
+PERCENTAGE_BACKGROUND_IMAGES = 20
 
 
 # ### Util Classes
@@ -106,7 +107,7 @@ def getSlicingWindow(img, startX, startY, M, N):
 # In[9]:
 
 
-def fillDataset(relativePath, patientIndex, timeFolder):
+def fillDataset(train_df, relativePath, patientIndex, timeFolder):
     global dataset
 
     timeIndex = timeFolder.replace(SAVE_REGISTERED_FOLDER+relativePath, '').replace("/", "")
@@ -122,9 +123,17 @@ def fillDataset(relativePath, patientIndex, timeFolder):
     imagesDict = {} # faster access to the images
     for imagename in np.sort(glob.glob(timeFolder+"*.png")): # sort the images !
         filename = imagename.replace(timeFolder, '')
-        if filename != "01.png": # don't take the first image (the manually annotated one)
-            image = cv2.imread(imagename, 0)
-            imagesDict[filename] = image
+        #if filename != "01.png": # don't take the first image (the manually annotated one)
+        image = cv2.imread(imagename, 0)
+        imagesDict[filename] = image
+
+    pixelsList, otherInforList = list(), list()
+    numRep = 1
+    if DATA_AUGMENTATION: numRep = 6
+
+    for rep in range(numRep):
+        pixelsList.append(dict())
+        otherInforList.append(dict())
 
     while True:
         if startingX>=IMAGE_WIDTH-M and startingY>=IMAGE_HEIGHT-N: # if we reach the end of the image, break the while loop.
@@ -162,28 +171,39 @@ def fillDataset(relativePath, patientIndex, timeFolder):
             numCore+=numReplication
 
         for data_aug_idx in range(numReplication):
-            tmparray = []
+            # tmparray = []
+
             for imagename in np.sort(glob.glob(timeFolder+"*.png")): # sort the images !
+                if str(startingX) not in pixelsList[data_aug_idx].keys(): pixelsList[data_aug_idx][str(startingX)] = dict()
+                if str(startingX) not in otherInforList[data_aug_idx].keys(): otherInforList[data_aug_idx][str(startingX)] = dict()
+                if str(startingY) not in pixelsList[data_aug_idx][str(startingX)].keys(): pixelsList[data_aug_idx][str(startingX)][str(startingY)] = list()
+                if str(startingY) not in otherInforList[data_aug_idx][str(startingX)].keys(): otherInforList[data_aug_idx][str(startingX)][str(startingY)] = dict()
+
                 filename = imagename.replace(timeFolder, '')
-                if filename != "01.png": # don't take the first image (the manually annotated one)
-                    image = imagesDict[filename]
-                    slicingWindow = getSlicingWindow(image, startingX, startingY, M, N)
+                #if filename != "01.png": # don't take the first image (the manually annotated one)
+                image = imagesDict[filename]
+                slicingWindow = getSlicingWindow(image, startingX, startingY, M, N)
 
-                    if data_aug_idx==1: slicingWindow = np.rot90(slicingWindow) # rotate 90 degree counterclockwise
-                    elif data_aug_idx==2: slicingWindow = np.rot90(slicingWindow,2) # rotate 180 degree counterclockwise
-                    elif data_aug_idx==3: slicingWindow = np.rot90(slicingWindow,3) # rotate 270 degree counterclockwise
-                    elif data_aug_idx==4: slicingWindow = np.flipud(slicingWindow) # flip the matrix up/down
-                    elif data_aug_idx==5: slicingWindow = np.fliplr(slicingWindow) # flip the matrix left/right
+                if data_aug_idx==1: slicingWindow = np.rot90(slicingWindow) # rotate 90 degree counterclockwise
+                elif data_aug_idx==2: slicingWindow = np.rot90(slicingWindow,2) # rotate 180 degree counterclockwise
+                elif data_aug_idx==3: slicingWindow = np.rot90(slicingWindow,3) # rotate 270 degree counterclockwise
+                elif data_aug_idx==4: slicingWindow = np.flipud(slicingWindow) # flip the matrix up/down
+                elif data_aug_idx==5: slicingWindow = np.fliplr(slicingWindow) # flip the matrix left/right
 
-                    tmparray.extend(slicingWindow)
+                # tmparray.extend(slicingWindow)
 
-            if classToSet==LABELS[0]:
-                backgroundPixelList.append(tmparray)
-                backgroundGroundTruthList.append(realLabelledWindow)
-            else:
-                tmpListPixels.append(tmparray)
-                tmpListGroundTruth.append(realLabelledWindow)
-                tmpListClasses.append(classToSet)
+                pixelsList[data_aug_idx][str(startingX)][str(startingY)].extend(slicingWindow)
+
+            otherInforList[data_aug_idx][str(startingX)][str(startingY)]["ground_truth"] = realLabelledWindow
+            otherInforList[data_aug_idx][str(startingX)][str(startingY)]["label_class"] = classToSet
+
+            # if classToSet==LABELS[0]:
+            #     backgroundPixelList.append(tmparray)
+            #     backgroundGroundTruthList.append(realLabelledWindow)
+            # else:
+            #     tmpListPixels.append(tmparray)
+            #     tmpListGroundTruth.append(realLabelledWindow)
+            #     tmpListClasses.append(classToSet)
 
         if startingY<IMAGE_HEIGHT-N: startingY += SLICING_PIXELS
         else:
@@ -198,19 +218,41 @@ def fillDataset(relativePath, patientIndex, timeFolder):
     print("\t\t\t Core: {0}".format(numCore))
     print("+++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
-    indices = random.sample(range(0,len(backgroundPixelList)), MAX_NUM_BACKGROUND_IMAGES)
-    newBackgroundPixelList, newBackgroundGroundTruthList = list(), list()
-    for index in indices:
-        newBackgroundPixelList.append(backgroundPixelList[index])
-        newBackgroundGroundTruthList.append(backgroundGroundTruthList[index])
+    print(train_df.shape)
 
-    print("\t\t\t Randomly picked {0} background images.".format(str(MAX_NUM_BACKGROUND_IMAGES)))
+    backElem = 0
+    for d in range(0, len(pixelsList)):
+        for x in pixelsList[d].keys():
+            for y in pixelsList[d][x].keys():
+                pixels = np.array(pixelsList[d][x][y]).reshape(NUMBER_OF_IMAGE_PER_SECTION,M,N)
+                label = otherInforList[d][x][y]["label_class"]
+                gt = otherInforList[d][x][y]["ground_truth"]
 
-    dataset[patientIndex]["data"] = tmpListPixels + newBackgroundPixelList
-    dataset[patientIndex]["ground_truth"] = tmpListGroundTruth + newBackgroundGroundTruthList
-    dataset[patientIndex]["label_class"] = tmpListClasses + [LABELS[0]]*MAX_NUM_BACKGROUND_IMAGES
+                perc = random.randint(0, 100)
+                if label==LABELS[0]:
+                    if perc > PERCENTAGE_BACKGROUND_IMAGES:
+                        continue
+                    else:
+                        backElem+=1
+                tmp_df = pd.DataFrame(np.array([[patientIndex, label, pixels, gt]]), columns=['patient_id', 'label', 'pixels', 'ground_truth'])
+                tmp_df['label_code'] = tmp_df.label.map({LABELS[0]:0, LABELS[1]:1, LABELS[2]:2, LABELS[3]:3})
+                train_df = train_df.append(tmp_df, ignore_index=True)
 
+    print(train_df.shape)
 
+    # indices = random.sample(range(0,len(backgroundPixelList)), MAX_NUM_BACKGROUND_IMAGES)
+    # newBackgroundPixelList, newBackgroundGroundTruthList = list(), list()
+    # for index in indices:
+    #     newBackgroundPixelList.append(backgroundPixelList[index])
+    #     newBackgroundGroundTruthList.append(backgroundGroundTruthList[index])
+
+    print("\t\t\t Randomly picked {0} background images (~ {1} %).".format(str(backElem), PERCENTAGE_BACKGROUND_IMAGES))
+
+    # dataset[patientIndex]["data"] = tmpListPixels + newBackgroundPixelList
+    # dataset[patientIndex]["ground_truth"] = tmpListGroundTruth + newBackgroundGroundTruthList
+    # dataset[patientIndex]["label_class"] = tmpListClasses + [LABELS[0]]*MAX_NUM_BACKGROUND_IMAGES
+
+    return train_df
 
 # Function that initialize the dataset: for each subfolder of the patient (section of the brain), it call the `fillDataset` function to get the pixels, save into the dataset and analyze them later.
 
@@ -219,38 +261,49 @@ def fillDataset(relativePath, patientIndex, timeFolder):
 
 def initializeDataset():
     patientFolders = glob.glob(SAVE_REGISTERED_FOLDER+"*/")
+
+    suffix_filename = "_"+str(SLICING_PIXELS)+"_"+str(M)+"x"+str(N)
+
     for numFold, patientFolder in enumerate(patientFolders): # for each patient
-        train_df = pd.DataFrame(columns=['patient_id', 'label', 'pixels', 'ground_truth']) # reset the dataframe for every patient
+        train_df = pd.DataFrame(columns=['patient_id', 'label', 'pixels', 'ground_truth', 'label_code']) # reset the dataframe for every patient
 
         relativePath = patientFolder.replace(SAVE_REGISTERED_FOLDER, '')
         patientIndex = relativePath.replace("PA", "").replace("/", "")
         #filename_train = SCRIPT_PATH+"trainComplete"+str(patientIndex)+".h5"
-        filename_train = SCRIPT_PATH+"patient"+str(patientIndex)+".hkl"
+        filename_train = SCRIPT_PATH+"newPatient"+str(patientIndex)+suffix_filename+".hkl"
         subfolders = glob.glob(patientFolder+"*/")
 
-        if int(patientIndex)==6 or int(patientIndex)==11:
+        if int(patientIndex)>1 and int(patientIndex)<=11:
             print("\t Analyzing {0}/{1}; patient folder: {2}...".format(numFold+1, len(patientFolders), relativePath))
             for count, timeFolder in enumerate(subfolders): # for each slicing time
                 initializeLabels(patientIndex)
                 print("\t\t Analyzing subfolder {0}".format(timeFolder.replace(SAVE_REGISTERED_FOLDER, '').replace(relativePath, '')))
                 start = time.time()
 
-                fillDataset(relativePath, patientIndex, timeFolder) # insert the data inside the dataset dictionary
-                print("\t\t Details:", [(key, len(subdataset)) for key, subdataset in dataset[patientIndex].items()])
+                train_df = fillDataset(train_df, relativePath, patientIndex, timeFolder) # insert the data inside the dataset dictionary
+                # print("\t\t Details:", [(key, len(subdataset)) for key, subdataset in dataset[patientIndex].items()])
+                print("+++++++++++++++++++++++++++++++++++++++++++++++++++++")
+                print("\t\t Background: {0}".format(str(sum(train_df.label==LABELS[0]))))
+                print("\t\t Brain: {0}".format(str(sum(train_df.label==LABELS[1]))))
+                print("\t\t Penumbra: {0}".format(str(sum(train_df.label==LABELS[2]))))
+                print("\t\t Core: {0}".format(str(sum(train_df.label==LABELS[3]))))
+                print("+++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
                 end = time.time()
                 print("\t\t Processed {0}/{1} subfolders in {2}s.".format(count+1, len(subfolders), round(end-start, 3)))
 
-                convertDatasetInList()
-                print("Preparing partial TRAIN dataframe...")
-                tmp = prepareTraining()
-
+                # start = time.time()
+                # convertDatasetInList()
+                # print("Preparing partial TRAIN dataframe...")
+                # tmp = prepareTraining()
+                # end = time.time()
                 # append the new rows in the dataframe
-                for index in range(0, tmp.shape[0]): train_df = train_df.append( tmp.iloc[index] )
+                # for index in range(0, tmp.shape[0]): train_df = train_df.append( tmp.iloc[index] )
+                # print("Time: {0}".format(round(end-start, 3)))
                 print("Train shape: ", train_df.shape)
             print("Saving TRAIN dataframe for patient {1} in {0}...".format(filename_train, str(patientIndex)))
-            suffix = "_DATA_AUGMENTATION" if DATA_AUGMENTATION else ""
-                #train_df.to_hdf(filename_train, key="X_"+str(M)+"x"+str(N)+"_"+str(SLICING_PIXELS) + suffix)
+            #suffix = "_DATA_AUGMENTATION" if DATA_AUGMENTATION else ""
+            #train_df.to_hdf(filename_train, key="X_"+str(M)+"x"+str(N)+"_"+str(SLICING_PIXELS) + suffix)
             hkl.dump(train_df, filename_train, mode='w')
 
 
@@ -261,15 +314,15 @@ def initializeDataset():
 def convertDatasetInList():
     global listPatientsDataset, dataset
     listPatientsDataset = {} # reset the list
-    #ID = 0
+
     for patient_id in dataset.keys():
         listPatientsDataset[patient_id] = []
         for idx, pixels in enumerate(dataset[patient_id]["data"]):
+            # print(np.array(pixels).shape)
             pixels = np.array(pixels).reshape(NUMBER_OF_IMAGE_PER_SECTION,M,N)
             ground_truth = dataset[patient_id]["ground_truth"][idx]
             label = dataset[patient_id]["label_class"][idx]
             listPatientsDataset[patient_id].append((patient_id, label, pixels, ground_truth))
-            #ID += 1
 
 
 # In[12]:
@@ -299,41 +352,9 @@ def prepareTraining():
 
 # ## Main
 
-# In[14]:
-
-
 if __name__ == '__main__':
     start = time.time()
     print("Initializing dataset...")
     initializeDataset()
     end = time.time()
     print("Total time: {0}s".format(round(end-start, 3)))
-
-
-# In[ ]:
-
-
-#print(train_df.shape)
-#train_df.tail(5)
-
-
-# In[ ]:
-
-
-#filename_train = SCRIPT_PATH+"trainComplete02.h5"
-#train_saved = pd.read_hdf(filename_train, "X_"+str(M)+"x"+str(N)+"_"+str(SLICING_PIXELS))
-
-
-# In[ ]:
-
-
-#train_saved.pixels.values[0]
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:

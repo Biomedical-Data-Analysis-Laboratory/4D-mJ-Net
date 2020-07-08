853b87ccaba41b663587e1e80f61f4f316c96809
tom501 <2921329@uis.no>
2020-07-08 13:11:20 +0200

    fix testing file for 3 classes

--- testing.py
+++ testing.py
@@ -20,26 +20,32 @@ def predictFromModel(nn, input):
 def predictAndSaveImages(that, p_id):
     start = time.time()
     stats = {}
+    suffix = general_utils.getSuffix() ## es == "_4_16x16"
+
+    filename_test = that.datasetFolder+constants.DATASET_PREFIX+str(p_id)+suffix+".pkl"
+    if not os.path.exists(filename_test):
+        if constants.getVerbose(): print("[WARNING] - File {} does NOT exist".format(filename_test))
+        return stats
 
     relativePatientFolder = constants.getPrefixImages()+p_id+"/"
     relativePatientFolderHeatMap = relativePatientFolder + "HEATMAP/"
     relativePatientFolderTMP = relativePatientFolder + "TMP/"
     patientFolder = that.patientsFolder+relativePatientFolder
-    general_utils.createDir(that.saveImagesFolder+that.getNNID(p_id)+general_utils.getSuffix())
-    general_utils.createDir(that.saveImagesFolder+that.getNNID(p_id)+general_utils.getSuffix()+"/"+relativePatientFolder)
-    general_utils.createDir(that.saveImagesFolder+that.getNNID(p_id)+general_utils.getSuffix()+"/"+relativePatientFolderHeatMap)
-    general_utils.createDir(that.saveImagesFolder+that.getNNID(p_id)+general_utils.getSuffix()+"/"+relativePatientFolderTMP)
+    general_utils.createDir(that.saveImagesFolder+that.getNNID(p_id)+suffix)
+    general_utils.createDir(that.saveImagesFolder+that.getNNID(p_id)+suffix+"/"+relativePatientFolder)
+    general_utils.createDir(that.saveImagesFolder+that.getNNID(p_id)+suffix+"/"+relativePatientFolderHeatMap)
+    general_utils.createDir(that.saveImagesFolder+that.getNNID(p_id)+suffix+"/"+relativePatientFolderTMP)
 
     if constants.getVerbose():
         for w in range(2): general_utils.printSeparation("-", 100)
 
     for subfolder in glob.glob(patientFolder+"*/"):
         try:
-            subpatientFolder = that.getNNID(p_id)+general_utils.getSuffix()+"/"+relativePatientFolder
-            patientFolderHeatMap = that.getNNID(p_id)+general_utils.getSuffix()+"/"+relativePatientFolderHeatMap
-            patientFolderTMP = that.getNNID(p_id)+general_utils.getSuffix()+"/"+relativePatientFolderTMP
+            subpatientFolder = that.getNNID(p_id)+suffix+"/"+relativePatientFolder
+            patientFolderHeatMap = that.getNNID(p_id)+suffix+"/"+relativePatientFolderHeatMap
+            patientFolderTMP = that.getNNID(p_id)+suffix+"/"+relativePatientFolderTMP
 
-            tmpStats = predictImage(that, subfolder, p_id, patientFolder, subpatientFolder, patientFolderHeatMap, patientFolderTMP)
+            tmpStats = predictImage(that, subfolder, p_id, patientFolder, subpatientFolder, patientFolderHeatMap, patientFolderTMP, filename_test)
 
             if that.save_statistics:
                 for func in that.statistics:
@@ -63,82 +69,32 @@ def predictAndSaveImages(that, p_id):
 
 ################################################################################
 # Generate a SINGLE image for the patient and save it
-def predictImage(that, subfolder, p_id, patientFolder, relativePatientFolder, relativePatientFolderHeatMap, relativePatientFolderTMP):
+def predictImage(that, subfolder, p_id, patientFolder, relativePatientFolder, relativePatientFolderHeatMap, relativePatientFolderTMP, filename_test):
     start = time.time()
     stats = {}
     YTRUEToEvaluate, YPREDToEvaluate = [], []
-    imagesDict = {} # faster access to the images
-    imagePredicted = np.zeros(shape=(constants.IMAGE_WIDTH, constants.IMAGE_HEIGHT), dtype=np.uint8)
-    checkImageProcessed = np.zeros(shape=(constants.IMAGE_WIDTH, constants.IMAGE_HEIGHT), dtype=np.uint8)
-    # imagePredicted = np.zeros(shape=(constants.IMAGE_WIDTH, constants.IMAGE_HEIGHT, 3), dtype=np.uint8)
-    suffix = general_utils.getSuffix() ## es == "_4_16x16"
+    imagePredicted = np.zeros(shape=(constants.IMAGE_WIDTH, constants.IMAGE_HEIGHT))
+    checkImageProcessed = np.zeros(shape=(constants.IMAGE_WIDTH, constants.IMAGE_HEIGHT))
 
-    idx = general_utils.getStringPatientIndex(subfolder.replace(patientFolder, '').replace("/", "")) # image index
+    idx = general_utils.getStringFromIndex(subfolder.replace(patientFolder, '').replace("/", "")) # image index
 
-    logsName = that.saveImagesFolder+relativePatientFolder+idx+"_logs.txt"
     # remove the old logs.
+    logsName = that.saveImagesFolder+relativePatientFolder+idx+"_logs.txt"
     if os.path.isfile(logsName): os.remove(logsName)
 
     if constants.getVerbose(): print("[INFO] - Analyzing Patient {0}, image {1}.".format(p_id, idx))
     s1 = time.time()
-    filename_test = that.datasetFolder+constants.DATASET_PREFIX+str(p_id)+suffix+".pkl"
+
     test_df = dataset_utils.loadSingleTrainingData(that.da, filename_test, p_id)
     test_df = test_df[test_df.data_aug_idx==0] # get only the rows with data_aug_idx==0 (no rotation or any data augmentation)
-    test_df = test_df[test_df.timeIndex==idx]
+    test_df = test_df[test_df.sliceIndex==idx] # get only the rows where the slice == idx
 
     if constants.getVerbose(): print("Rows in dataframe to analyze: {}".format(len(test_df.index)))
-    for i_t, test_row in test_df.iterrows(): # for every rows of the same image
-        startingX, startingY = test_row.x_y
-        pixels = test_row.pixels.reshape(1, test_row.pixels.shape[0], test_row.pixels.shape[1], test_row.pixels.shape[2], 1)
 
-        # get the label image only if the path is set
-        if that.labeledImagesFolder!="":
-            y_true = test_row.ground_truth
-            # cv2.imwrite(that.saveImagesFolder+relativePatientFolderTMP+idx+"y_true.png", y_true)
-        # for q in range(test_row.pixels.shape[0]):  cv2.imwrite(that.saveImagesFolder+relativePatientFolderTMP+idx+"_"+str(q)+"real.png", test_row.pixels[q,:,:])
-
-        ### MODEL PREDICT (image & statistics)
-
-        # slicingWindowPredicted contain only the prediction for the last step
-        slicingWindowPredicted = predictFromModel(that, pixels)[that.test_steps-1]
-        multiplier = 255
-
-        if that.to_categ:
-            slicingWindowPredicted = K.eval((K.argmax(slicingWindowPredicted)*256)/len(constants.LABELS))
-            multiplier = 1
-
-        if that.save_statistics and that.labeledImagesFolder!="":
-            YTRUEToEvaluate.extend(y_true)
-            YPREDToEvaluate.extend(slicingWindowPredicted*multiplier)
-
-        if that.save_images:
-            threeDimensionSlicingWindow = np.zeros(shape=(slicingWindowPredicted.shape[0],slicingWindowPredicted.shape[1]), dtype=np.uint8)
-            # threeDimensionSlicingWindow = np.zeros(shape=(slicingWindowPredicted.shape[0],slicingWindowPredicted.shape[1], 3), dtype=np.uint8)
-
-            if constants.N_CLASSES == 4:
-                thresBack = constants.PIXELVALUES[0]
-                thresBrain = constants.PIXELVALUES[1]
-                thresPenumbra = constants.PIXELVALUES[2]
-                thresCore = constants.PIXELVALUES[3]
-                eps1, eps2, eps3 = that.epsilons[0]
-            else:
-                thresBack = constants.PIXELVALUES[0]
-                thresCore = constants.PIXELVALUES[1]
-                eps1 = that.epsilons[0]
-
-            # print(startingX,startingX+constants.getM(),startingY,startingY+constants.getN())
-            # print(slicingWindowPredicted.shape)
-            # print(slicingWindowPredicted)
-
-            imagePredicted[startingX:startingX+constants.getM(), startingY:startingY+constants.getN()] = slicingWindowPredicted*multiplier
-            checkImageProcessed[startingX:startingX+constants.getM(), startingY:startingY+constants.getN()] = 255
-            # for r, _ in enumerate(slicingWindowPredicted):
-            #     threeDimensionSlicingWindow[r] = slicingWindowPredicted[r]*multiplier
-
-                # for c, pixel in enumerate(slicingWindowPredicted[r]):
-                #     threeDimensionSlicingWindow[r][c] = (pixel*multiplier,)*3
-            # Create the image
-            # imagePredicted[startingX:startingX+constants.getM(), startingY:startingY+constants.getN()] = threeDimensionSlicingWindow
+    if constants.get3DFlag()!="": imagePredicted, checkImageProcessed, YTRUEToEvaluate, YPREDToEvaluate = generateTimeImagesAndConsensus(that, test_df, YTRUEToEvaluate, YPREDToEvaluate, relativePatientFolderTMP, idx)
+    else:
+        for _, test_row in test_df.iterrows(): # for every rows of the same image
+            imagePredicted, checkImageProcessed, YTRUEToEvaluate, YPREDToEvaluate = generate2DImage(that, test_row, imagePredicted, checkImageProcessed, YTRUEToEvaluate, YPREDToEvaluate)
 
     s2 = time.time()
     if constants.getVerbose(): print("image time: {}".format(round(s2-s1, 3)))
@@ -149,10 +105,12 @@ def predictImage(that, subfolder, p_id, patientFolder, relativePatientFolder, re
         # save the image predicted in the specific folder
         cv2.imwrite(that.saveImagesFolder+relativePatientFolder+idx+".png", imagePredicted)
         # create and save the HEATMAP
-        heatmap_img = cv2.applyColorMap(~imagePredicted, cv2.COLORMAP_JET)
+        # heatmap_img = cv2.applyColorMap(~imagePredicted, cv2.COLORMAP_JET)
+        heatmap_img = cv2.normalize(imagePredicted, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        heatmap_img = cv2.applyColorMap(~heatmap_img, cv2.COLORMAP_JET)
         cv2.imwrite(that.saveImagesFolder+relativePatientFolderHeatMap+idx+"_heatmap.png", heatmap_img)
 
-        cv2.imwrite(that.saveImagesFolder+relativePatientFolderTMP+idx+".png", checkImageProcessed)
+        if constants.get3DFlag()=="": cv2.imwrite(that.saveImagesFolder+relativePatientFolderTMP+idx+".png", checkImageProcessed)
 
         s2 = time.time()
         if constants.getVerbose(): print("save time: {}".format(round(s2-s1, 3)))
@@ -172,9 +130,8 @@ def predictImage(that, subfolder, p_id, patientFolder, relativePatientFolder, re
             if classToEval not in fp.keys(): fp[classToEval] = {}
             if classToEval not in tp.keys(): tp[classToEval] = {}
 
-            if that.epsiloList[0]!=None:
+            if that.epsiloList[0]!=None: # loop over the various epsilon
                 for  idxE,percEps in enumerate(that.epsiloList):
-                    # loop over the various epsilon
                     tn[classToEval][idxE], fn[classToEval][idxE], fp[classToEval][idxE], tp[classToEval][idxE] = metrics.mappingPrediction(YTRUEToEvaluate, YPREDToEvaluate, that.use_background_in_statistics, that.epsilons, percEps, label)
 
         for func in that.statistics:
@@ -202,15 +159,102 @@ def predictImage(that, subfolder, p_id, patientFolder, relativePatientFolder, re
     return stats
 
 ################################################################################
+# Generate a 2D image from the test_df
+# Return:
+#       - imagePredicted: the predicted image
+#       - checkImageProcessed: the ground truth image
+#       - YTRUEToEvaluate: list for stats corresponding to the ground truth image
+#       - YPREDToEvaluate: list for stats corresponding to the predicted image
+def generate2DImage(that, test_row, imagePredicted, checkImageProcessed, YTRUEToEvaluate, YPREDToEvaluate):
+    startingX, startingY = test_row.x_y
+    if constants.get3DFlag()=="": pixels = test_row.pixels.reshape(1, test_row.pixels.shape[0], test_row.pixels.shape[1], test_row.pixels.shape[2], 1)
+    else: pixels = test_row.pixels.reshape(1, test_row.pixels.shape[0], test_row.pixels.shape[1], test_row.pixels.shape[2])
+
+    # get the label image only if the path is set
+    if that.labeledImagesFolder!="": y_true = test_row.ground_truth
+
+    # slicingWindowPredicted contain only the prediction for the last step
+    slicingWindowPredicted = predictFromModel(that, pixels)[that.test_steps-1]
+    multiplier = 255
+
+    if that.to_categ:
+        slicingWindowPredicted = K.eval((K.argmax(slicingWindowPredicted)*256)/len(constants.LABELS))
+        multiplier = 1
+
+    slicingWindowPredicted *= multiplier
+
+    if that.save_statistics and that.labeledImagesFolder!="":
+        YPREDToEvaluate.extend(slicingWindowPredicted)
+        YTRUEToEvaluate.extend(y_true)
+
+    if that.save_images:
+        threeDimensionSlicingWindow = np.zeros(shape=(slicingWindowPredicted.shape[0],slicingWindowPredicted.shape[1]), dtype=np.uint8)
+
+        if constants.N_CLASSES == 4:
+            thresBack = constants.PIXELVALUES[0]
+            thresBrain = constants.PIXELVALUES[1]
+            thresPenumbra = constants.PIXELVALUES[2]
+            thresCore = constants.PIXELVALUES[3]
+            eps1, eps2, eps3 = that.epsilons[0]
+        else:
+            thresBack = constants.PIXELVALUES[0]
+            thresCore = constants.PIXELVALUES[1]
+            eps1 = that.epsilons[0]
+
+        imagePredicted[startingX:startingX+constants.getM(), startingY:startingY+constants.getN()] = slicingWindowPredicted
+        checkImageProcessed[startingX:startingX+constants.getM(), startingY:startingY+constants.getN()] = y_true
+        # for r, _ in enumerate(slicingWindowPredicted):
+        #     threeDimensionSlicingWindow[r] = slicingWindowPredicted[r]*multiplier
+
+            # for c, pixel in enumerate(slicingWindowPredicted[r]):
+            #     threeDimensionSlicingWindow[r][c] = (pixel*multiplier,)*3
+        # Create the image
+        # imagePredicted[startingX:startingX+constants.getM(), startingY:startingY+constants.getN()] = threeDimensionSlicingWindow
+
+    return (imagePredicted, checkImageProcessed, YTRUEToEvaluate, YPREDToEvaluate)
+
+################################################################################
+# Generate the image from the 3D sequence of time index (create also these images)
+# with a consensus
+def generateTimeImagesAndConsensus(that, test_df, YTRUEToEvaluate, YPREDToEvaluate, relativePatientFolderTMP, idx):
+    imagePredicted = np.zeros(shape=(constants.IMAGE_WIDTH, constants.IMAGE_HEIGHT))
+    checkImageProcessed = np.zeros(shape=(constants.IMAGE_WIDTH, constants.IMAGE_HEIGHT))
+    arrayTimeIndexImages = dict()
+
+    for _, test_row in test_df.iterrows(): # for every rows of the same image
+        if str(test_row.timeIndex) not in arrayTimeIndexImages.keys(): arrayTimeIndexImages[str(test_row.timeIndex)] = np.zeros(shape=(constants.IMAGE_WIDTH, constants.IMAGE_HEIGHT), dtype=np.uint8)
+        arrayTimeIndexImages[str(test_row.timeIndex)], checkImageProcessed, YTRUEToEvaluate, YPREDToEvaluate = generate2DImage(that, test_row, arrayTimeIndexImages[str(test_row.timeIndex)], checkImageProcessed, YTRUEToEvaluate, YPREDToEvaluate)
+
+    if that.save_images:
+        if constants.N_CLASSES==3:
+            checkImageProcessed[checkImageProcessed==255] = constants.PIXELVALUES[0] # remove one class from the ground truth
+            checkImageProcessed[checkImageProcessed==150] == constants.PIXELVALUES[2] # change the class for core
+        cv2.imwrite(that.saveImagesFolder+relativePatientFolderTMP+"orig_"+idx+".png", checkImageProcessed)
+
+        for tidx in arrayTimeIndexImages.keys():
+            curr_image = arrayTimeIndexImages[tidx]
+            # save the images
+            cv2.imwrite(that.saveImagesFolder+relativePatientFolderTMP+idx+"_"+general_utils.getStringFromIndex(tidx)+".png", curr_image)
+            # add the predicted image in the imagePredicted for consensus
+            imagePredicted += curr_image
+
+        imagePredicted /= len(arrayTimeIndexImages.keys())
+
+    return (imagePredicted, checkImageProcessed, YTRUEToEvaluate, YPREDToEvaluate)
+
+################################################################################
 # Test the model with the selected patient
 def evaluateModel(nn, p_id, isAlreadySaved):
     suffix = general_utils.getSuffix()
     if isAlreadySaved:
         filename_train = nn.datasetFolder+constants.DATASET_PREFIX+str(p_id)+suffix+".pkl"
+
+        if not os.path.exists(filename_train): return
+
         nn.train_df = dataset_utils.readFromPickle(filename_train)
         nn.dataset = dataset_utils.getTestDataset(nn.dataset, nn.train_df, p_id, nn.mp)
         nn.dataset["test"]["labels"] = dataset_utils.getLabelsFromIndex(train_df=nn.train_df, dataset=nn.dataset["test"], modelname=nn.name, to_categ=nn.to_categ, flag="train")
-        nn.compileModel() # compile the model and then evaluate
+        nn.compileModel() # compile the model and then evaluate it
 
     sample_weights = nn.getSampleWeights("test")
 
@@ -224,18 +268,19 @@ def evaluateModel(nn, p_id, isAlreadySaved):
         use_multiprocessing=nn.mp
     )
 
-    general_utils.printSeparation("-",50)
-    if not isAlreadySaved:
-        for metric_name in nn.train.history:
-            print("TRAIN %s: %.2f%%" % (metric_name, round(float(nn.train.history[metric_name][-1]), 6)*100))
-    for index, val in enumerate(testing):
-        print("TEST %s: %.2f%%" % (nn.model.metrics_names[index], round(val,6)*100))
-    general_utils.printSeparation("-",50)
-
-    with open(general_utils.getFullDirectoryPath(nn.saveTextFolder)+nn.getNNID(p_id)+suffix+".txt", "a+") as text_file:
+    if not nn.train_on_batch:
+        general_utils.printSeparation("-",50)
         if not isAlreadySaved:
             for metric_name in nn.train.history:
-                text_file.write("TRAIN %s: %.2f%% \n" % (metric_name, round(float(nn.train.history[metric_name][-1]), 6)*100))
+                print("TRAIN %s: %.2f%%" % (metric_name, round(float(nn.train.history[metric_name][-1]), 6)*100))
         for index, val in enumerate(testing):
-            text_file.write("TEST %s: %.2f%% \n" % (nn.model.metrics_names[index], round(val,6)*100))
-        text_file.write("----------------------------------------------------- \n")
+            print("TEST %s: %.2f%%" % (nn.model.metrics_names[index], round(val,6)*100))
+        general_utils.printSeparation("-",50)
+
+        with open(general_utils.getFullDirectoryPath(nn.saveTextFolder)+nn.getNNID(p_id)+suffix+".txt", "a+") as text_file:
+            if not isAlreadySaved:
+                for metric_name in nn.train.history:
+                    text_file.write("TRAIN %s: %.2f%% \n" % (metric_name, round(float(nn.train.history[metric_name][-1]), 6)*100))
+            for index, val in enumerate(testing):
+                text_file.write("TEST %s: %.2f%% \n" % (nn.model.metrics_names[index], round(val,6)*100))
+            text_file.write("----------------------------------------------------- \n")
